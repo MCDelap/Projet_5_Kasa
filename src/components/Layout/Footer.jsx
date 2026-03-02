21e09ecc2f24f698fef54eceec24adfe88d89b70
MClaire D <mcdelap@gmail.com>
2026-03-02 13:22:10 +0100

    Ajout des composants Header (avec navigation) et Footer (avec logo et copyright) au layout du site

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -4,8 +4,8 @@ import './Layout.scss';
 function Footer() {
   return (
     <footer className="footer">
-        <img src={logo} alt="logo Kasa"/>
-        <p>© 2020 Kasa. All rights reserved</p>
+        <img src={logo} alt="logo Kasa" className="footer__logo" />
+        <p className="footer__copyright">© 2020 Kasa. All rights reserved</p>
     </footer>
   );
 }
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,10 +5,10 @@ import './Layout.scss';
 function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="logo Kasa"/>
-      <nav className="nav">
-        <NavLink to="/" className="nav-link">Accueil</NavLink>
-        <NavLink to="/about" className="nav-link">À propos</NavLink>
+      <img src={logo} alt="logo Kasa" className="header__logo"/>
+      <nav className="header__nav">
+        <NavLink to="/" className="header__nav-link">Accueil</NavLink>
+        <NavLink to="/about" className="header__nav-link">À propos</NavLink>
       </nav>
     </header>
   );
