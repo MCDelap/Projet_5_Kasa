aeb7c9a15a2e190a2b36e5a7b763dc2647e5605d
MClaire D <mcdelap@gmail.com>
2026-03-04 10:25:56 +0100

    Mise en place de la page d'accueil et des composants associés

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,27 @@
-function Home() {
-  return <h1>Chez vous, partout et ailleurs</h1>;
+import Banner from '../../components/Banner'
+import Card from '../../components/Card'
+import data from '../../data/logements.json'
+import imageHome from '../../assets/home_banner.png'
+import './Home.scss'
+
+const Home = () => {
+  return (
+    <section className="home-container">
+    <Banner image={imageHome} title="Chez vous, partout et ailleurs" />
+    <div className="cards-grid">
+    {data.map(logement => (
+      <Card 
+        key={logement.id} 
+        id={logement.id} 
+        title={logement.title} 
+        cover={logement.cover} 
+        className="card"
+      />
+    ))}
+  </div>
+</section>
+)
 }
 
-export default Home; 
+export default Home;
+ 
