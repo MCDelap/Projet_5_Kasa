1cfcbfd39af26302862662ab8613475edaa32dd7
MClaire D <mcdelap@gmail.com>
2026-03-13 13:18:38 +0100

    Mise en place de la page A propos et du composant Collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react';
-import arrowAbout from '../../assets/dropdown_arrow.svg';
+import { useState } from 'react'
+import arrowAbout from '../../assets/dropdown_arrow.svg'
+import './Collapse.scss'
 
 function Collapse({ title, content }) {
   // Un seul state pour savoir si c'est ouvert ou non
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
-  const arrowClass = isOpen ? "arrow rotate" : "arrow";
-  const contentClass = isOpen ? "collapse_content show" : "collapse_content hide";
+  const arrowClass = `collapse__arrow ${isOpen ? "collapse__arrow--rotate" : ""}`
+  const contentClass = `collapse__content ${isOpen ? "collapse__content--show" : "collapse__content--hide"}`
 
   return (
-    <div className="collapse_item">
-      <div className="collapse_header" onClick={() => setIsOpen(!isOpen)}>
-        <h2>{title}</h2>
+    <div className="collapse">
+      <div className="collapse__header" onClick={() => setIsOpen(!isOpen)}>
+        <h2 className="collapse__title">{title}</h2>
         <img src={arrowAbout} alt="Ouvrir/Fermer" className={arrowClass} />
       </div>
       
       <div className={contentClass}>
-        {content}
+        <div className="collapse__text">
+          {content}
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Collapse;
+export default Collapse
