603f59d2a9797bf030f84627985ad0b7d602f9b4
MClaire D <mcdelap@gmail.com>
2026-03-13 13:50:09 +0100

    Mise en place de la page Erreur 404

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom'
+import imageError from '../../assets/error_404.png'
+import './Error.scss'
+
 function Error() {
-  return <h1>Erreur</h1>;
+  return (
+    <main className="error">
+      <img src={imageError} alt="Erreur 404" className="error__image" />
+
+      <p className="error__message">Oups ! La page que vous demandez n'existe pas.</p>
+      
+      <Link to="/" className="error__link">
+        Retourner sur la page d'accueil
+      </Link>
+    </main>
+  )
 }
 
-export default Error; 
+export default Error
