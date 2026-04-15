33be86fb639cd845800f0cae0092e9666df24148
MClaire D <mcdelap@gmail.com>
2026-04-15 18:18:25 +0200

    Harmonisation du code avec Prettier

--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -1,48 +1,60 @@
-import { useParams, Navigate } from 'react-router-dom'
-import data from '../../data/logements.json'
+import { useParams, Navigate } from "react-router-dom";
+import data from "../../data/logements.json";
 
-import Slideshow from '../../components/Slideshow/Slideshow'
-import Collapse from '../../components/Collapse/Collapse'
-import './Accommodation.scss'
+import Slideshow from "../../components/Slideshow/Slideshow";
+import Collapse from "../../components/Collapse/Collapse";
+import "./Accommodation.scss";
 
-import starActive from '../../assets/star-active.svg'
-import starInactive from '../../assets/star-inactive.svg'
+import starActive from "../../assets/star-active.svg";
+import starInactive from "../../assets/star-inactive.svg";
 
-function Accommodation () {
-  const { id } = useParams()
-  const logement = data.find(item => item.id === id)
+function Accommodation() {
+  const { id } = useParams();
+  const logement = data.find((item) => item.id === id);
 
   if (!logement) {
-    return <Navigate to="/404" replace />
+    return <Navigate to="/404" replace />;
   }
 
   return (
     <main className="accommodation">
       <Slideshow pictures={logement.pictures} />
 
-    <section className="accommodation__header">
+      <section className="accommodation__header">
         <div>
           <h1 className="accommodation__title">{logement.title}</h1>
           <p className="accommodation__location">{logement.location}</p>
           <div className="accommodation__tags">
             {logement.tags.map((tag, index) => (
-              <span key={index} className="accommodation__tag">{tag}</span>
+              <span key={index} className="accommodation__tag">
+                {tag}
+              </span>
             ))}
           </div>
         </div>
-        
+
         <div className="accommodation__host-and-rating">
           <div className="accommodation__host">
             <p className="accommodation__host-name">{logement.host.name}</p>
-            <img className="accommodation__host-picture" src={logement.host.picture} alt={logement.host.name} />
+            <img
+              className="accommodation__host-picture"
+              src={logement.host.picture}
+              alt={logement.host.name}
+            />
           </div>
 
           <div>
             {[1, 2, 3, 4, 5].map((star) => (
               <img
                 key={star}
-                src={star <= parseInt(logement.rating) ? starActive : starInactive}
-                alt={star <= parseInt(logement.rating) ? "Étoile active" : "Étoile inactive"}
+                src={
+                  star <= parseInt(logement.rating) ? starActive : starInactive
+                }
+                alt={
+                  star <= parseInt(logement.rating)
+                    ? "Étoile active"
+                    : "Étoile inactive"
+                }
                 className="accommodation__star"
               />
             ))}
@@ -52,17 +64,19 @@ function Accommodation () {
 
       <section className="accommodation__collapses">
         <Collapse title="Description" content={logement.description} />
-        <Collapse title="Equipements" content={
-          <ul>
-            {logement.equipments.map((item, index) => (
-              <li key={index}>{item}</li>
-            ))}
-          </ul>
-        } />
+        <Collapse
+          title="Equipements"
+          content={
+            <ul>
+              {logement.equipments.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          }
+        />
       </section>
     </main>
-  )
+  );
 }
 
-export default Accommodation
-
+export default Accommodation;
--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,19 +1,21 @@
-import { Link } from 'react-router-dom'
-import imageError from '../../assets/error_404.png'
-import './Error.scss'
+import { Link } from "react-router-dom";
+import imageError from "../../assets/error_404.png";
+import "./Error.scss";
 
 function Error() {
   return (
     <main className="error">
       <img src={imageError} alt="Erreur 404" className="error__image" />
 
-      <p className="error__message">Oups ! La page que vous demandez n'existe pas.</p>
-      
+      <p className="error__message">
+        Oups ! La page que vous demandez n'existe pas.
+      </p>
+
       <Link to="/" className="error__link">
         Retourner sur la page d'accueil
       </Link>
     </main>
-  )
+  );
 }
 
-export default Error
+export default Error;
