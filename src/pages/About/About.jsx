5d319f5fe4518b317978309edc2ae152b38df204
MClaire D <mcdelap@gmail.com>
2026-04-15 15:32:06 +0200

    Harmonisation du code avec Prettier

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,30 +1,34 @@
-import Banner from '../../components/Banner/Banner'
-import Collapse from '../../components/Collapse/Collapse'
-import './About.scss'
+import Banner from "../../components/Banner/Banner";
+import Collapse from "../../components/Collapse/Collapse";
+import "./About.scss";
 
-import imageAbout from '../../assets/about_banner.png'
+import imageAbout from "../../assets/about_banner.png";
 
 function About() {
-const aboutData = [
+  const aboutData = [
     {
       id: "1",
       title: "Fiabilité",
-      content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+      content:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
     },
     {
       id: "2",
       title: "Respect",
-      content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+      content:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
     },
     {
       id: "3",
       title: "Service",
-      content: "La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance."
+      content:
+        "La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.",
     },
     {
       id: "4",
       title: "Sécurité",
-      content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
     },
   ];
 
@@ -33,18 +37,14 @@ const aboutData = [
       <div className="about__banner">
         <Banner image={imageAbout} />
       </div>
-    
-    <div className="about__collapses">
-        {aboutData.map(item => (
-          <Collapse
-            key={item.id} 
-            title={item.title} 
-            content={item.content}
-          />
+
+      <div className="about__collapses">
+        {aboutData.map((item) => (
+          <Collapse key={item.id} title={item.title} content={item.content} />
         ))}
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,27 +1,27 @@
-import Banner from '../../components/Banner/Banner'
-import Card from '../../components/Card/Card'
-import './Home.scss'
+import Banner from "../../components/Banner/Banner";
+import Card from "../../components/Card/Card";
+import "./Home.scss";
 
-import data from '../../data/logements.json'
-import imageHome from '../../assets/home_banner.png'
+import data from "../../data/logements.json";
+import imageHome from "../../assets/home_banner.png";
 
-function Home () {
+function Home() {
   return (
-  <section className="home">
-    <Banner image={imageHome} title="Chez vous, partout et ailleurs" />
-    <div className="home__cards-grid">
-    {data.map(logement => (
-      <Card 
-        key={logement.id} 
-        id={logement.id} 
-        title={logement.title} 
-        cover={logement.cover} 
-      />
-    ))}
-    </div>
-  </section>
-)
+    <section className="home">
+      <Banner image={imageHome} title="Chez vous, partout et ailleurs" />
+
+      <div className="home__cards-grid">
+        {data.map((logement) => (
+          <Card
+            key={logement.id}
+            id={logement.id}
+            title={logement.title}
+            cover={logement.cover}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Home
- 
+export default Home;
